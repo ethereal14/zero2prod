0b06abc331fa478b5b24ed7981e0a12dabf459fc
ethereal14 <1097282916@qq.com>
2025-02-22 21:35:46 +0800

    实现subscribe接口，并且完成post请求的解析

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,6 +4,7 @@
 //
 // 你可以使用以下命令检查生成了哪些代码
 // cargo expand --test health_check
+use std::net::TcpListener;
 #[tokio::test]
 async fn health_check_works() {
     // 准备
@@ -23,6 +24,8 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+/// 启动应用程序的一个实例
+/// 并返回其地址(例如：http://localhost:XXXX)
 fn spawn_app() -> String {
     let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // 检索操作系统分配的端口
@@ -34,4 +37,53 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
     // 将应用程序地址返回给调用者
     format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // 准备
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // 执行
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // 断言
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // 准备
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the name"),
+        ("email=ursula_le_guin%40gmail.com", "missing the email"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        // 断言
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Requset when the payload was {}.",
+            error_message
+        );
+    }
 }
