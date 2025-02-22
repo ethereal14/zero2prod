68d733963f312ff848dde2c08f476d36f00772a1
ethereal14 <1097282916@qq.com>
2025-02-22 19:50:17 +0800

    优化：优化集成测试、优化服务器创建流程

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use zero2prod::run;
+use std::net::TcpListener;
 
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -7,13 +7,13 @@
 #[tokio::test]
 async fn health_check_works() {
     // 准备
-    spawn_app().await.expect("Failed to spawn our app.");
+    let address = spawn_app();
     // 需要引入reqWest对应用程序执行Http请求
     let client = reqwest::Client::new();
 
     // 执行
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execite request.");
@@ -23,6 +23,15 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> std::io::Result<()> {
-    todo!()
+fn spawn_app() -> String {
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // 检索操作系统分配的端口
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    // 启动服务器作为后台任务
+    // tokio::spawn 返回一个指向spawned future的handle
+    // 但是这里没有用它，因为这是非绑定的let用法
+    let _ = tokio::spawn(server);
+    // 将应用程序地址返回给调用者
+    format!("http://127.0.0.1:{}", port)
 }
